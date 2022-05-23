57504df747048e62f3d607fc6ec0082aed4952a8
yean1 <yean1@DESKTOP-9SUDEJ1>
2022-05-23 17:52:40 +0900

    定数ファイルを作成

---
+++ src/main/java/constants/AttributeConst.java
@@ -0,0 +1,69 @@
+package constants;
+
+public enum AttributeConst {
+  //フラッシュメッセージ
+    FLUSH("flush"),
+
+    //一覧画面共通
+    MAX_ROW("maxRow"),
+    PAGE("page"),
+
+    //入力フォーム共通
+    TOKEN("_token"),
+    ERR("errors"),
+
+    //ログイン中の従業員
+    LOGIN_EMP("login_employee"),
+
+    //ログイン画面
+    LOGIN_ERR("loginError"),
+
+    //従業員管理
+    EMPLOYEE("employee"),
+    EMPLOYEES("employees"),
+    EMP_COUNT("employees_count"),
+    EMP_ID("id"),
+    EMP_CODE("code"),
+    EMP_PASS("password"),
+    EMP_NAME("name"),
+    EMP_ADMIN_FLG("admin_flag"),
+
+    //管理者フラグ
+    ROLE_ADMIN(1),
+    ROLE_GENERAL(0),
+
+    //削除フラグ
+    DEL_FLAG_TRUE(1),
+    DEL_FLAG_FALSE(0),
+
+
+    MANAGEMENT("management"),
+    MANAGEMENTS("managements"),
+    MAN_COUNT("managements_count"),
+    MAN_ID("id"),
+    MAN_DATE("management_date"),
+    MAN_TITLE("title"),
+    MAN_CONTENT("content");
+
+    private final String text;
+    private final Integer i;
+
+    private AttributeConst(final String text) {
+        this.text = text;
+        this.i = null;
+    }
+
+    private AttributeConst(final Integer i) {
+        this.text = null;
+        this.i = i;
+    }
+
+    public String getValue() {
+        return this.text;
+    }
+
+    public Integer getIntegerValue() {
+        return this.i;
+    }
+
+}
